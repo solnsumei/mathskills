1313b8c9e9c8353ab37814a2e91f5e12cc614601
solnsumei <solnsumei@gmail.com>
2020-12-25 00:36:11 +0100

    Refactor help page to include descriptive icons

--- lib/src/core/utils/constants.dart
+++ lib/src/core/utils/constants.dart
@@ -1,9 +1,18 @@
 const List<String> INSTRUCTIONS = [
-  "Select one or more numbers from the numbers frame that will sum up to the value of the cups displayed.",
-  "Click on the = button to check your answer. If the answer is wrong, you can withdraw your answers by clicking on the selected numbers or select new numbers from the numbers frame. If the answer is correct, the button will display a green check mark.",
-  "Click on the check button to accept the answer and go to next round.",
-  "If there is no number or possible combinations of numbers that sum up to the number of displayed, you can click on the redraw button to choose another random number of cups. Note: You have only 5 redraws for each game session",
-  "Repeat the first 3 steps until you have selected all the numbers from the numbers frame. You win the game when you successfully use all the numbers and you lose the game when you have numbers left unused after exhausting the redraws.",
+  "Select one or more numbers from the numbers frame that will sum up to the number of items displayed at the top",
+  "Click on the",
+  "images/equals.png",
+  "button to check your answer",
+  "If the answer is wrong, you can withdraw your answers by clicking on the selected numbers. If the answer is correct, the button will display a green check mark",
+  "Click on the",
+  "images/green_check.png",
+  "button to accept the answer and go to next round",
+  "If there is no number or possible combinations of numbers that sum up to the number of items displayed, you can click on the redraw button",
+  "images/redraw_img.png",
+  "to choose another random number of items",
+  "Note: You have only 5 redraws for each game session",
+  "Repeat the previous steps until you have selected all the numbers from the numbers frame",
+  "You win the game when you successfully use all the numbers and you lose the game when you have numbers left unused after exhausting the redraws",
   "When the game ends, either win or lose, click on the try again button to start a new game session. Have fun!"
 ];
 
--- lib/src/ui/screens/help_page.dart
+++ lib/src/ui/screens/help_page.dart
@@ -3,6 +3,47 @@ import '../../core/core.dart';
 
 class HelpPage extends StatelessWidget {
 
+  List<Widget> populateList(List items) {
+    final List<Widget> itemsWidget = [];
+
+    final listWithImage = [];
+    
+    items.asMap().forEach((index, value) {
+      final widgetItem = ListTile(
+        subtitle: Text(value),
+      );
+      
+      switch(index) {
+        case 1:
+        case 5:
+          listWithImage.add(widgetItem);
+          break;
+        case 2:
+        case 7:
+          listWithImage.add(
+            Image(image: AssetImage(value)),
+          );
+          break;
+        case 3:
+        case 8:
+          listWithImage.add(widgetItem);
+          itemsWidget.add(
+            ListTile(
+              subtitle: Row(
+                children: List<Widget>.from(listWithImage),
+              ),
+            )
+          );
+          listWithImage.clear();
+          break;
+        default:
+          itemsWidget.add(widgetItem);
+      }
+    });
+    
+    return itemsWidget;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,15 +58,68 @@ class HelpPage extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView.builder(
-        padding: EdgeInsets.symmetric(vertical: 12.0),
-        itemCount: INSTRUCTIONS.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text("Step ${index + 1}"),
-            subtitle: Text(INSTRUCTIONS[index]),
-          );
-        },
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 12.0),
+          children: [
+            ListTile(
+              subtitle: Text(INSTRUCTIONS[0]),
+            ),
+            ListTile(
+              subtitle: Row(
+                children: [
+                  Text(INSTRUCTIONS[1]),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                    child: Image(image: AssetImage(INSTRUCTIONS[2]), width: 30.0),
+                  ),
+                  Expanded(child: Text(INSTRUCTIONS[3]), flex: 2,),
+                ],
+              ),
+            ),
+            ListTile(
+              subtitle: Text(INSTRUCTIONS[4]),
+            ),
+            ListTile(
+              subtitle: Row(
+                children: [
+                  Text(INSTRUCTIONS[5]),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                    child: Image(image: AssetImage(INSTRUCTIONS[6]), width: 30.0),
+                  ),
+                  Expanded(child: Text(INSTRUCTIONS[7]), flex: 2,),
+                ],
+              ),
+            ),
+            ListTile(
+              subtitle: Text(INSTRUCTIONS[8]),
+            ),
+            ListTile(
+              subtitle: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                    child: Image(image: AssetImage(INSTRUCTIONS[9]), width: 50.0),
+                  ),
+                  Expanded(child: Text(INSTRUCTIONS[10]), flex: 2,),
+                ],
+              ),
+            ),
+            ListTile(
+              subtitle: Text(INSTRUCTIONS[11]),
+            ),
+            ListTile(
+              subtitle: Text(INSTRUCTIONS[12]),
+            ),
+            ListTile(
+              subtitle: Text(INSTRUCTIONS[13]),
+            ),
+            ListTile(
+              subtitle: Text(INSTRUCTIONS[14]),
+            ),
+          ],
+        ),
       ),
     );
   }
