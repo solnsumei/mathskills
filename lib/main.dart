690160e09dc21a86a57c9f701b01ddc046c77613
solnsumei <solnsumei@gmail.com>
2020-12-24 18:28:46 +0100

    Ensure game is played in portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'src/my_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(MyApp());
 }
 
